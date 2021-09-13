b582e5ded7437474ca8ecd8f52d43ca2312ae6f9
Robson José Lopes Gonçalves Júnior <robsonlopes.jr@gmail.com>
2021-09-13 10:47:09 -0300

    filtrando dados usando o filter e mostrando na tela com o map

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import JSONDATA from './db/Data.json';
+
+import './App.css';
 
 const App: React.FC = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
-    <div>
-      <h1>Inicio</h1>
+    <div className="App">
+      <input type="text" name="search" id="search" placeholder="Search..." onChange={event => { setSearchTerm(event.target.value) }} />
+      {JSONDATA.filter((val) => {
+        if (searchTerm === "") {
+          return val
+        } else if (val.author.toLowerCase().includes(searchTerm.toLowerCase())) {
+          return val
+        }
+      }).map((val, key) => {
+        return (
+          <div className="user" key={key}>
+            <p>
+              {val.author}
+            </p>
+          </div>
+        )
+      })}
     </div>
   );
 }
